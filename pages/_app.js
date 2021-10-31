3290f84a3c85631553efb9453b7eda98c50a2222
Lachlan Kemp <me@lachlankemp.com>
2021-10-31 11:31:28 +1030

    added loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,34 @@ import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
 import { ThemeProvider } from 'next-themes'
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
+import '/styles/nprogress.css'
+
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`)
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
   return (
     <ThemeProvider attribute="class" defaultTheme="dark"> 
       <Component {...pageProps} />
