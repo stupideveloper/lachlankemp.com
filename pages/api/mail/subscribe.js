64d21c0203110fbecf0e57aabac5ca59e3e40b6f
Lachlan Kemp <me@lachlankemp.com>
2021-10-31 18:40:53 +1030

    changed mail provider

--- pages/api/mail/subscribe.js
+++ pages/api/mail/subscribe.js
@@ -1,24 +1,23 @@
-import mailchimp from '@mailchimp/mailchimp_marketing';
-
-mailchimp.setConfig({
-	apiKey: process.env.MAILCHIMP_API_KEY,
-	server: process.env.MAILCHIMP_API_SERVER
-});
-
+const apiKey = process.env.CONVERTKIT_API_KEY;
+const formId = process.env.CONVERTKIT_FORM_ID;
 const subscribe = async (req, res) => {
 	const { email } = req.body;
-
 	if (!email) {
 		return res.status(400).json({ error: 'Email is required' });
 	}
-
 	try {
-		await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
-			email_address: email,
-			status: 'subscribed'
+		const response = await fetch(`https://api.convertkit.com/v3/forms/${formId}/subscribe`, { 
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json; charset=utf-8'
+			},
+			body: JSON.stringify({
+				"api_key": apiKey,
+				"email": email
+			})
 		});
-
-		return res.status(201).json({ error: '' });
+		const data = await response.json();
+		return res.status(201).json({ error: '', createdAt: data.subscription.created_at, state: data.subscription.state });
 	} catch (error) {
 		return res.status(500).json({ error: error.message || error.toString() });
 	}
