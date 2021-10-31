8aa8e3f952e45cd75c76ffe64344755b8bb02d74
Lachlan Kemp <me@lachlankemp.com>
2021-10-31 20:27:12 +1030

    added funky animations

---
+++ pages/mail/successful-signup.js
@@ -0,0 +1,21 @@
+import BaseContainer from "/components/Containers/BaseContainer";
+import BaseLayout from "/components/Containers/BaseLayout";
+import Head from "next/head";
+export default function Test() {
+	return (
+		<BaseLayout>
+			<Head>
+				<title>Subscribed – Lachlan Kemp</title>
+				<meta name="description" content="A person focusing on, Javascript, React, Svelte and new technologies." />
+				<link rel="canonical" href="https://lachlankemp.com/mail/successful-signup" />
+			</Head>
+			<BaseContainer>
+				<div>
+					<h1 className="text-5xl">Successfuly Subscribed</h1>
+          <h2 className="mt-4">Thanks for signing up, you will now receive emails from me!</h2>
+				</div>
+				
+			</BaseContainer>
+		</BaseLayout>
+	);
+}
