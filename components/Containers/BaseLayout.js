518fa0f4ce9b6a7d455846b05db7b54410e06286
Lachlan Kemp <me@lachlankemp.com>
2022-01-12 22:50:48 +1030

    Start Fresh

--- components/Containers/BaseLayout.js
+++
@@ -1,3 +0,0 @@
-export default function BaseLayout({children}) {
-	return <div className="dark:text-white dark:bg-black flex-col items-center justify-center min-h-screen">{children}</div>;
-}
--- pages/_app.js
+++ pages/_app.js
@@ -1,52 +1,7 @@
-import 'tailwindcss/tailwind.css';
-import '../styles/global.css';
-import { ThemeProvider } from 'next-themes';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import NProgress from 'nprogress';
-import '/styles/nprogress.css';
-
-import splitbee from '@splitbee/web';
-
-
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-
-	useEffect(() => {
-		splitbee.init({
-			scriptUrl: "/bee.js",
-			apiUrl: "/_hive",
-		});
-	}, []);
-
-	const router = useRouter();
-
-	useEffect(() => {
-		const handleStart = (url) => {
-			console.log(`Loading: ${url}`);
-			NProgress.start();
-		};
-		const handleStop = () => {
-			NProgress.done();
-		};
-
-		router.events.on('routeChangeStart', handleStart);
-		router.events.on('routeChangeComplete', handleStop);
-		router.events.on('routeChangeError', handleStop);
-
-		return () => {
-			router.events.off('routeChangeStart', handleStart);
-			router.events.off('routeChangeComplete', handleStop);
-			router.events.off('routeChangeError', handleStop);
-		};
-	}, [router]);
-
-	return (
-		<ThemeProvider attribute="class"> 
-			<Component {...pageProps} />
-		</ThemeProvider>
-	);
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
