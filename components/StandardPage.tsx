5e95f621148b211669e5532be8ef32c3128c1bc8
Lachlan Kemp <me@lachlankemp.com>
2022-01-27 13:33:48 +1030

    whoops

--- components/StandardPage.tsx
+++ components/StandardPage.tsx
@@ -1,7 +1,7 @@
 import Head from "./Head"
 import MainWrapper from "./MainWrapper"
 import Header from "./sections/Header"
-export default function StandardPage({url, image, description, title, children} : {url?: string, image?: string, description?: string, title: string, children: React.ReactElement}) {
+export default function StandardPage({url, image, description, title, children} : {url?: string, image?: string, description?: string, title: string, children: any}) {
 	return (
 		<div className="bg-gray-50 ">
 			<Head url={url} image={image} description={description} title={title} />
